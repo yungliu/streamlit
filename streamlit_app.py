4f92329082006b81ce8909fdbd5a620fd2ecae05
yungliu <yung.liu@it-ce.fr>
2022-11-28 17:10:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,4 @@
 import streamlit as st
 st.title('Bienvenue')
+st.text_area(label, value="", height=None, max_chars=None, key=None, help=None, on_change=None, args=None, kwargs=None, *, placeholder=None, disabled=False, label_visibility="visible")
+click = st.button("Resume")
