88de3fefbbf06d3a8626dfd6c78dc1fe42b823d6
yungliu <yung.liu@it-ce.fr>
2022-11-28 17:32:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,7 @@ txtInput1=st.text_area('textinput1', value="Mon test sur Streamlit")
 click = st.button("Resume")
 
 url = 'https://azfpowerapps.azurewebsites.net/api/httptriggerpowerapps'
-myobj = {'name': 'It was the best of times. it was the worst of times, it wasthe age of wisdom. it was the age of foolishness. it was the epoch of belief. it was the epoch of incredulity. i was the season of Light. it was the season of Darkness. itwas the spring of hope, it was the winter of despair. (...)'}
-
+myobj = {'name':txtInput1}
 st.write('envoi',myobj)
 
 x = requests.post(url, json = myobj)
