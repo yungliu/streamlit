90d83359b177b82c8698879c07d14773035d40bc
yungliu <yung.liu@it-ce.fr>
2022-11-28 17:19:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import reuests
 st.title('Bienvenue')
 txtInput1=st.text_area('textinput1', value="Mon test sur Streamlit")
 click = st.button("Resume")
+
+url = 'https://azfpowerapps.azurewebsites.net/api/httptriggerpowerapps'
+myobj = {'name': 'It was the best of times, it was the worst of times, it wasthe age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, i was the season of Light, it was the season of Darkness, itwas the spring of hope, it was the winter of despair, (...)'}
+
+x = requests.post(url, json = myobj)
+
+print(x.text)
