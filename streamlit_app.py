081659e7247bc00c085712e5f32538e81ad9d1a2
yungliu <yung.liu@it-ce.fr>
2023-01-10 15:02:42 +0100

    Update streamlit_app.py
    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,21 +5,17 @@ st.title('Bienvenue')
 txtInput1=st.text_area('textinput1', value="Mon test sur Streamlit")
 txtInputAPPKEY = st.text_input('appKey',value="Appkey")
 click = st.button("Resume")
-
-url = 'https://<xxxxxxxx<w'
-myobj = {'name':txtInput1}
-st.write('envoi',myobj)
-
-openai.api_key=txtInputAPPKEY
-response = openai.Completion.create(
+  openai.api_key=txtInputAPPKEY
+  response = openai.Completion.create(
   model="code-davinci-002",
   prompt="##### Fix bugs in the below function\n \n### Buggy Python\nimport Random\na = random.randint(1,12)\nb = random.randint(1,12)\nfor i in range(10):\n    question = \"What is \"+a+\" x \"+b+\"? \"\n    answer = input(question)\n    if answer = a*b\n        print (Well done!)\n    else:\n        print(\"No.\")\n    \n### Fixed Python",
-  temperature=0,
-  max_tokens=182,
-  top_p=1.0,
-  frequency_penalty=0.0,
-  presence_penalty=0.0,
-  stop=["###"]
-)
+   temperature=0,
+   max_tokens=182,
+   top_p=1.0,
+   frequency_penalty=0.0,
+   presence_penalty=0.0,
+   stop=["###"]
+  )
+  st.write('Le Résume ', response.text)
 
-st.write('Le Résume ', response.text)
+ 
