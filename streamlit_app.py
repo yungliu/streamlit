e70ae7a065d33e1e763cb4e5ae33b58be9369f97
yungliu <yung.liu@it-ce.fr>
2023-01-14 23:58:11 +0100

    MAJ Hauteur

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import openai
 
 st.title('Bienvenue Stephane')
-txtInput1=st.text_area('textinput1', value="##### Fix bugs in the below function\n \n### Buggy Python\nimport Random\na = random.randint(1,12)\nb = random.randint(1,12)\nfor i in range(10):\n    question = \"What is \"+a+\" x \"+b+\"? \"\n    answer = input(question)\n    if answer = a*b\n        print (Well done!)\n    else:\n        print(\"No.\")\n    \n### Fixed Python")
+txtInput1=st.text_area('textinput1', value="##### Fix bugs in the below function\n \n### Buggy Python\nimport Random\na = random.randint(1,12)\nb = random.randint(1,12)\nfor i in range(10):\n    question = \"What is \"+a+\" x \"+b+\"? \"\n    answer = input(question)\n    if answer = a*b\n        print (Well done!)\n    else:\n        print(\"No.\")\n    \n### Fixed Python",height=200)
 txtInputAPPKEY = st.text_input('appKey',value="Appkey")
 if st.button("Action"):
   openai.api_key=txtInputAPPKEY
