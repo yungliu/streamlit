b9b60394fa9bbfe90f3c5f5d0a45aac930080d33
yungliu <yung.liu@it-ce.fr>
2023-01-23 15:50:32 +0100

    MAJ pres

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,12 @@ with st.sidebar:
 
 txtInput1=st.text_area('Code à Analyser', value="### Fix bugs in the below function\n \n### Buggy Python\nimport Random\na = random.randint(1,12)\nb = random.randint(1,12)\nfor i in range(10):\n    question = \"What is \"+a+\" x \"+b+\"? \"\n    answer = input(question)\n    if answer = a*b\n        print (Well done!)\n    else:\n        print(\"No.\")\n    \n### Fixed Python",height=300)
 txtInputAPPKEY = st.text_input('appKey',value="Appkey")
+openai.api_type = "azure"
+openai.api_base = "https://tsi-openai.openai.azure.com/"
+openai.api_version = "2022-12-01"
+openai.api_key ="d7d2408f7f2144499cb314b66ad59f6f" 
+#os.getenv("OPENAI_API_KEY")
+
 if st.button("Action"):
   ##  openai.api_key=txtInputAPPKEY
   ##  response = openai.Completion.create(
@@ -50,21 +56,16 @@ if st.button("Action"):
   ##  )
 #  openai.api_key=txtInputAPPKEY
 
-openai.api_type = "azure"
-openai.api_base = "https://tsi-openai.openai.azure.com/"
-openai.api_version = "2022-12-01"
-openai.api_key ="d7d2408f7f2144499cb314b66ad59f6f" 
-#os.getenv("OPENAI_API_KEY")
 
-response = openai.Completion.create(
-  engine="de-code-davinci-002",
-  prompt="### Postgres SQL tables, with their properties:\n#\n# Employee(id, name, department_id)\n# Department(id, name, address)\n# Salary_Payments(id, employee_id, amount, date)\n#\n### A query to list the names of the departments which employed more than 10 employees in the last 3 months\n\nSELECT",
-  temperature=0,
-  max_tokens=150,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0,
-  stop=["#",";"])
-st.write('Le Retour ', response.choices[0].text)
+  response = openai.Completion.create(
+    engine="de-code-davinci-002",
+    prompt="### Postgres SQL tables, with their properties:\n#\n# Employee(id, name, department_id)\n# Department(id, name, address)\n# Salary_Payments(id, employee_id, amount, date)\n#\n### A query to list the names of the departments which employed more than 10 employees in the last 3 months\n\nSELECT",
+    temperature=0,
+    max_tokens=150,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0,
+    stop=["#",";"])
+  st.write('Le Retour ', response.choices[0].text)
 
  
