87b35f1854d6b3ec6c8e4a68d4730e3253c22cd5
yungliu <yung.liu@it-ce.fr>
2023-01-23 15:45:16 +0100

    ZZ

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,17 +49,21 @@ if st.button("Action"):
   ##   stop=["###"]
   ##  )
   openai.api_key=txtInputAPPKEY
-  response = openai.Completion.create(
-   model="code-davinci-002",
-   prompt=txtInput1,
-   temperature=0,
-   max_tokens=182,
-   top_p=1.0,
-   frequency_penalty=0.0,
-   presence_penalty=0.0,
-   stop=["###"]
-  )
 
+openai.api_type = "azure"
+openai.api_base = "https://stsopenai.openai.azure.com/"
+openai.api_version = "2022-12-01"
+#openai.api_key = os.getenv("OPENAI_API_KEY")
+
+response = openai.Completion.create(
+  engine="de-code-davinci-002",
+  prompt="### Postgres SQL tables, with their properties:\n#\n# Employee(id, name, department_id)\n# Department(id, name, address)\n# Salary_Payments(id, employee_id, amount, date)\n#\n### A query to list the names of the departments which employed more than 10 employees in the last 3 months\n\nSELECT",
+  temperature=0,
+  max_tokens=150,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0,
+  stop=["#",";"])
   st.write('Le Retour ', response.choices[0].text)
 
  
